f7275efdbaf0b1be0c5c14008ce1cd77d6b3fb08
hassan-kaa <hassan.kaabechi@insat.ucar.tn>
2024-06-28 10:48:13 +0100

    fixed root layout typing for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import Provider from "./contexts/AuthContext";
-import { Session } from "next-auth";
+import { Session, getServerSession } from "next-auth";
 import { ReactNode } from "react";
+import options from "./api/auth/[...nextauth]/options";
 
 const poppins = Poppins({
   weight: ["100", "300", "400", "500", "700"],
@@ -14,12 +15,16 @@ export const metadata = {
   description: "A journaling app that uses AI to analyze your mood.",
 };
 
-interface RootLayoutProps {
-  children: ReactNode;
-  session: Session;
-}
-
-export default function RootLayout({ children, session }: RootLayoutProps) {
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const session: Session | null = await getServerSession(options);
+  if (!session) {
+    // Handle the case when session is null
+    return null;
+  }
   return (
     <html lang="en">
       <body className={poppins.className}>
