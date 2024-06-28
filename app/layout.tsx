efa2ead5958a5adbd8d306e483bb31cff148dd54
hassan-kaa <hassan.kaabechi@insat.ucar.tn>
2024-06-28 10:37:35 +0100

    fixed typing for root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,22 +2,24 @@ import "./globals.css";
 import { Poppins } from "next/font/google";
 import Provider from "./contexts/AuthContext";
 import { Session } from "next-auth";
+import { ReactNode } from "react";
+
 const poppins = Poppins({
   weight: ["100", "300", "400", "500", "700"],
   subsets: ["latin"],
 });
+
 export const metadata = {
   title: "Mood-ai",
   description: "A journaling app that uses AI to analyze your mood.",
 };
 
-export default function RootLayout({
-  children,
-  session,
-}: {
-  children: React.ReactNode;
+interface RootLayoutProps {
+  children: ReactNode;
   session: Session;
-}) {
+}
+
+export default function RootLayout({ children, session }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={poppins.className}>
