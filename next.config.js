c8f65502e4720fbd15eaa2a3a99d3fdd9e8f1ac7
hassan-kaa <hassan.kaabechi@insat.ucar.tn>
2024-07-08 10:20:51 +0100

    base_url and cors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["https://icons8.com/icon/2802/brain"],
+    domains: [
+      "https://icons8.com/icon/2802/brain",
+      "https://mood-ai-sage.vercel.app",
+    ],
   },
 };
 
